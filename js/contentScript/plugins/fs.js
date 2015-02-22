082418d07da74403403c382e7def2401644e9648
Anatolii Yakushko <shaddyx@gmail.com>
2015-02-22 21:00:38 +0200

    [-] fixed single-file add button, [+] added clear list/show list buttons, [+] added fs.ua plugin

---
+++ js/contentScript/plugins/fs.js
@@ -0,0 +1,21 @@
+/**
+ * Created by shaddy on 16.02.15.
+ */
+console.log("ex loaded");
+
+ContentScriptUtils.addMessageListener("getItemList", function(){
+    console.log("received getItemList message");
+    var elements = document.querySelectorAll("a[href^='/get/'][title]");
+    var playerItems= [];
+    for (var k in elements){
+        if (elements[k].tagName === "A"){
+            var item = new PlayerItem(elements[k].getAttribute("title"), "http://ex.ua" + elements[k].getAttribute("href"));
+            //item.setUseCaptionFromLocation(true);
+            item.setUseUrlFromLocation(true);
+            if (item.caption.endsWith(".avi") || item.caption.endsWith(".mkv") || item.caption.endsWith(".flv") || item.caption.endsWith(".mp4") || item.caption.endsWith(".xvid")){
+                playerItems.push(item);
+            }
+        }
+    }
+    ContentScriptUtils.addPlayerItems(playerItems);
+});
